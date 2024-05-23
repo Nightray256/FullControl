3c05905ac7629db93d9cf902ce01e8793a5b3c39
NIGHT <jimmy20090503@gmail.com>
2024-05-23 19:35:12 +0800

    update

--- src/main/java/nr/FPC.java
+++ src/main/java/nr/FPC.java
@@ -5,6 +5,7 @@ import nr.lockchest.command.LCCommand;
 import nr.lockchest.listener.LockChest;
 import nr.pvp.PVPCommand;
 import nr.rtp.RTPCommand;
+import nr.tpa.TPACommand;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -16,6 +17,7 @@ public final class FPC extends JavaPlugin {
         this.getCommand("pvp").setExecutor(new PVPCommand());
         this.getCommand("rtp").setExecutor(new RTPCommand());
         this.getCommand("lockchest").setExecutor(new LCCommand());
+        this.getCommand("tpa").setExecutor(new TPACommand());
 
         getServer().getPluginManager().registerEvents(new LockChest(), this);
     }
